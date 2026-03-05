09d5a014773fc9d43cbeff36c45216f2fbc673cd
Shenne Magtolis <magtolishen0813@gmail.com>
2026-03-05 11:36:48 +0000

    File changes

--- src/components/layout/MobileNav.jsx
+++ src/components/layout/MobileNav.jsx
@@ -48,7 +48,7 @@ export default function MobileNav({ unreadMessages = 0, profile }) {
 
       {/* Slide-out Panel */}
       <div
-        className={`fixed top-0 left-0 h-full w-72 z-50 bg-[#0f0f1a] border-r border-white/10 shadow-2xl transform transition-transform duration-300 ease-in-out md:hidden ${
+        className={`fixed top-0 left-0 h-full w-72 z-50 bg-[#0f0f1a] border-r border-white/10 shadow-2xl transform transition-transform duration-300 ease-in-out ${
           open ? "translate-x-0" : "-translate-x-full"
         }`}
       >
